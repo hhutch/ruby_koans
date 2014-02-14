29e12ca471b82f69de499eafce5b19d4658c4aa2
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2014-02-13 23:03:08 -0500

    161 tests passing,, a days worth of work

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,8 +13,19 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+require 'set'
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  group = [a,b,c]
+  count = group.map {|x| x.abs}.to_set.length
+  # count = [a-c, a-b, b-c].map {|x| x.abs}.to_set # if we were testing given points and calculating the length
+  raise TriangleError, "Sides have to be greater than zero" if group.to_set === [0].to_set
+  raise TriangleError, "Sides have to be a positive number" if group.any? {|x| x<0}
+  raise TriangleError, "Two sides can never be less than the sum of one side" if ((a+b)<c || (a+c)<b || (c+b)<a)
+  raise TriangleError, "Two sides can never be  equal one side" if ((a+b)==c || (a+c)==b || (c+b)==a)
+  return :equilateral if count == 1
+  return :isosceles if count == 2
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
